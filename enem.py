63565e3e334aa6dc9249d0750539307e521dfdca
634751 <daniel.limapo@pcdf.df.gov.br>
2023-05-22 14:28:42 -0300

    updating branch

--- enem.py
+++ enem.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-import pandas as pd
 import matplotlib
-
-microDadosENEM = pd.read_csv('./dados/microdados_enem_2021/DADOS/MICRODADOS_ENEM_2021.csv', sep=";", encoding="ISO-8859-1")
-
-print(microDadosENEM.head())
-
-=======
 import time
 import pandas as pd
 from sqlalchemy import create_engine
@@ -61,4 +53,8 @@ print("COPY duration: {} seconds".format(time.time() - start_time))
 
 # close connection
 conn.close()
->>>>>>> 91670a3667c5c322b0cc7b89fa85e6f18c738bc8
+
+microDadosENEM = pd.read_csv(
+    './dados/microdados_enem_2021/DADOS/MICRODADOS_ENEM_2021.csv', sep=";", encoding="ISO-8859-1")
+
+print(microDadosENEM.head())
