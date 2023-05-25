50a34681f78fce69e4ef42187ea3be49e93d4942
Daniel Lim-Apo <daniel.github@sede.com.br>
2023-05-24 22:16:47 -0300

    All tables with populate code in correct order considering foreign keys

---
+++ delete-tables-content.sql
@@ -0,0 +1,7 @@
+delete from tb_redacao;
+delete from tb_prova_objetiva;
+delete from tb_prova;
+delete from tb_municipio;
+delete from tb_questionario_socioeconomico;
+delete from tb_escola_participante;
+delete from tb_participante;
--- enem-populate-tables.py
+++ enem-populate-tables.py
@@ -15,42 +15,42 @@ from populate.tb_prova_objetiva_MT import populate_table_tb_prova_objetiva_MT
 from populate.tb_redacao import populate_table_tb_redacao
 
 # INPUT YOUR OWN CONNECTION STRING HERE
-conn_string = 'postgresql://postgres:z77zpoqq@localhost:5432/enem'
+conn_string = 'postgresql://postgres:yourpass@localhost:5432/enem'
 # print(os.getcwd())
 # enem_2021 = pd.read_csv('dados\microdados_enem_2021\DADOS\DadosByUF\MICRODADOS_ENEM_2021_AC.csv',
 #                         nrows=10000, encoding='iso-8859-1', sep=',')
 # enem_2021 = pd.read_csv('dados\microdados_enem_2021\DADOS\DadosByUF\MICRODADOS_ENEM_2021_AC.csv',
 #                         nrows=10000, encoding='utf-8', sep=',')
 enem_2021 = pd.read_csv(
-    'dados\microdados_enem_2021\DADOS\DadosByUF\MICRODADOS_ENEM_2021_AC.csv', encoding='utf-8', sep=',')
+    'dados\microdados_enem_2021\DADOS\MICRODADOS_ENEM_2021.csv', encoding='latin-1', sep=';')
 
-# populate_table_tb_participante(conn_string, enem_2021, 'tb_participante')
+populate_table_tb_municipio(
+    conn_string, enem_2021, 'tb_municipio')
+
+populate_table_tb_participante(conn_string, enem_2021, 'tb_participante')
 
 populate_table_tb_escola_participante(
     conn_string, enem_2021, 'tb_escola_participante')
 
-# populate_table_tb_questionario_socioeconomico(
-#     conn_string, enem_2021, 'tb_questionario_socioeconomico')
+populate_table_tb_questionario_socioeconomico(
+    conn_string, enem_2021, 'tb_questionario_socioeconomico')
 
-# populate_table_tb_municipio(
-#     conn_string, enem_2021, 'tb_municipio')
+populate_table_tb_prova(
+    conn_string, enem_2021, 'tb_prova')
 
-# populate_table_tb_prova(
-#     conn_string, enem_2021, 'tb_prova')
+populate_table_tb_prova_objetiva_CN(
+    conn_string, enem_2021, 'tb_prova_objetiva')
 
-# populate_table_tb_prova_objetiva_CN(
-#     conn_string, enem_2021, 'tb_prova_objetiva')
+populate_table_tb_prova_objetiva_CH(
+    conn_string, enem_2021, 'tb_prova_objetiva')
 
-# populate_table_tb_prova_objetiva_CH(
-#     conn_string, enem_2021, 'tb_prova_objetiva')
+populate_table_tb_prova_objetiva_LC(
+    conn_string, enem_2021, 'tb_prova_objetiva')
 
-# populate_table_tb_prova_objetiva_LC(
-#     conn_string, enem_2021, 'tb_prova_objetiva')
+populate_table_tb_prova_objetiva_MT(
+    conn_string, enem_2021, 'tb_prova_objetiva')
 
-# populate_table_tb_prova_objetiva_MT(
-#     conn_string, enem_2021, 'tb_prova_objetiva')
-
-# populate_table_tb_redacao(
-#     conn_string, enem_2021, 'tb_redacao')
+populate_table_tb_redacao(
+    conn_string, enem_2021, 'tb_redacao')
 
 print("End of popluating tables")
